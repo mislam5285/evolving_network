f120c44341c52353f6e6f07cced895b958010918
Swami Iyer <swami.iyer@gmail.com>
2016-08-03 10:47:45 -0400

    changed subplots to individual plots and removed community vs epochs plot

--- plots.py
+++ plots.py
@@ -17,17 +17,15 @@ def main(args):
     y1, Y1, Y1ERR = [], [], [] # robustness
     y2, Y2, Y2ERR = [], [], [] # degree variance
     y3, Y3, Y3ERR = [], [], [] # avg. path length
-    y4, Y4, Y4ERR = [], [], [] # communities
-    y5, Y5, Y5ERR = [], [], [] # clustering
-    y6, Y6, Y6ERR = [], [], [] # assortativity
+    y4, Y4, Y4ERR = [], [], [] # clustering
+    y5, Y5, Y5ERR = [], [], [] # assortativity
     while len(a[i]) > 0:
         toks = map(float, a[i].pop(0).strip().split())
         y1.append(toks[0])
         y2.append(toks[1])
         y3.append(toks[2])
-        y4.append(toks[3])
-        y5.append(toks[4])
-        y6.append(toks[5])
+        y4.append(toks[4])
+        y5.append(toks[5])
         i += 1
         if i == len(a):
             i = 0
@@ -46,65 +44,60 @@ def main(args):
             Y5.append(numpy.mean(y5))
             Y5ERR.append(numpy.std(y5))
             y5 = []
-            Y6.append(numpy.mean(y6))
-            Y6ERR.append(numpy.std(y6))
-            y6 = []
 
     T = range(len(Y1))
     font_prop = font_manager.FontProperties(size = 12)
 
-    pylab.figure(1, figsize = (21, 15), dpi = 500)
 
     # robustness vs epoch
-    pylab.subplot(3, 2, 1)
+    pylab.figure(1, figsize = (5, 7), dpi = 500)
     pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
     pylab.ylabel(r"robustness $R$", fontproperties = font_prop)
     #pylab.errorbar(T, Y1, Y1ERR, color = "k", fmt = ".")
     pylab.plot(T, Y1, "k-", linewidth = 2, alpha = 0.6)
     pylab.xlim(0, max(T))
+    pylab.savefig("robustness.pdf", format = "pdf", bbox_inches = "tight")
+    pylab.close(1)
 
     # degree variance vs epoch
-    pylab.subplot(3, 2, 2)
+    pylab.figure(2, figsize = (5, 7), dpi = 500)
     pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
     pylab.ylabel(r"degree variance $Var(k)$", fontproperties = font_prop)
     #pylab.errorbar(T, Y2, Y2ERR, color = "k", fmt = ".")
     pylab.plot(T, Y2, "k-", linewidth = 2, alpha = 0.6)
     pylab.xlim(0, max(T))
+    pylab.savefig("degree_variance.pdf", format = "pdf", bbox_inches = "tight")
+    pylab.close(2)
 
     # average path length vs epoch
-    pylab.subplot(3, 2, 3)
+    pylab.figure(3, figsize = (5, 7), dpi = 500)
     pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
     pylab.ylabel(r"average path length $\ell$", fontproperties = font_prop)
     #pylab.errorbar(T, Y3, Y3ERR, color = "k", fmt = ".")
     pylab.plot(T, Y3, "k-", linewidth = 2, alpha = 0.6)
     pylab.xlim(0, max(T))
-
-    # number of communities vs epoch
-    pylab.subplot(3, 2, 4)
-    pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
-    pylab.ylabel(r"number of communities $\kappa$", fontproperties = font_prop)
-    #pylab.errorbar(T, Y4, Y6ERR, color = "k", fmt = ".")
-    pylab.plot(T, Y4, "k-", linewidth = 2, alpha = 0.6)
-    pylab.xlim(0, max(T))
+    pylab.savefig("average_path_length.pdf", format = "pdf", bbox_inches = "tight")
+    pylab.close(3)
 
     # clustering coefficient vs epoch
-    pylab.subplot(3, 2, 5)
+    pylab.figure(4, figsize = (5, 7), dpi = 500)
     pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
     pylab.ylabel(r"clustering coefficient $C$", fontproperties = font_prop)
+    #pylab.errorbar(T, Y4, Y4ERR, color = "k", fmt = ".")
+    pylab.plot(T, Y4, "k-", linewidth = 2, alpha = 0.6)
+    pylab.xlim(0, max(T))
+    pylab.savefig("clustering_coefficient.pdf", format = "pdf", bbox_inches = "tight")
+    pylab.close(4)
+
+    # assortativity coefficient vs epoch
+    pylab.figure(5, figsize = (5, 7), dpi = 500)
+    pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
+    pylab.ylabel(r"assortativity coefficient $r$", fontproperties = font_prop)
     #pylab.errorbar(T, Y5, Y5ERR, color = "k", fmt = ".")
     pylab.plot(T, Y5, "k-", linewidth = 2, alpha = 0.6)
     pylab.xlim(0, max(T))
-
-    # assortativity coefficient vs epoch
-    pylab.subplot(3, 2, 6)
-    pylab.xlabel(r"epoch $t$", fontproperties = font_prop)
-    pylab.ylabel(r"assortativity coefficient $r$", fontproperties = font_prop)
-    #pylab.errorbar(T, Y6, Y4ERR, color = "k", fmt = ".")
-    pylab.plot(T, Y6, "k-", linewidth = 2, alpha = 0.6)
-    pylab.xlim(0, max(T))
-
-    pylab.savefig("plots.pdf", format = "pdf", bbox_inches = "tight")
-    pylab.close(1)
+    pylab.savefig("assortativity_coefficient.pdf", format = "pdf", bbox_inches = "tight")
+    pylab.close(5)
 
 if __name__ == "__main__":
     main(sys.argv[1:])
